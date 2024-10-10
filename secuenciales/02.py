afb95ff52f6fc425294fad9a1c8ac975ff3915be
CristianQSS8113 <cristianeduardo1812@gmail.com>
2024-10-09 22:32:56 -0500

    pytonnn

---
+++ secuenciales/02.py
@@ -0,0 +1,11 @@
+medida= int ( input ( " Ingrese metros : " ) )
+
+centimetros = medida * 100
+pulgadas = centimetros / 2.54
+pie = pulgadas / 12
+yarda = pie/ 3
+
+print(f"{centimetros}centimetros")
+print(f"{pulgadas} pulgadas")
+print(f"{pie}pie")
+print(f"{yarda}yarda")
