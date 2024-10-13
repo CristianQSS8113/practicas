04e22c9405daa86fe063e2649227d3bf528a87e5
CristianQSS8113 <cristianeduardo1812@gmail.com>
2024-10-12 22:05:40 -0500

    13

---
+++ secuenciales/13.py
@@ -0,0 +1,12 @@
+import os
+os.system ("cls")
+
+import math
+
+a=float(input("Valor de A: "))
+b=float(input("Valor de B: "))
+
+hipotenusa=math.sqrt(a**2 + b**2)
+
+print(f"La hipotenusa es: {hipotenusa}")
+
