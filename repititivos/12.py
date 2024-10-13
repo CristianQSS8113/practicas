e365507f3e37d9f1f41cb29bd5b71e0df2af3f7a
CristianQSS8113 <cristianeduardo1812@gmail.com>
2024-10-13 13:16:39 -0500

    12 y 13

--- repititivos/12.py
+++ repititivos/12.py
@@ -1,2 +1,14 @@
 import os 
 os.system("cls")
+
+def mostrar_numeros():
+    for i in range(1, 101):
+        print(f"{i:2}", end=' ')
+        if i % 10 == 0:
+            print()  
+
+def main():
+    mostrar_numeros()
+
+if __name__ == "__main__":
+    main()
