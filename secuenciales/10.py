ba9cc9685c98f9988b08bc2317eeb622b6c3d800
CristianQSS8113 <cristianeduardo1812@gmail.com>
2024-10-12 19:33:40 -0500

    10

---
+++ secuenciales/10.py
@@ -0,0 +1,8 @@
+import os
+os.system ("cls")
+
+numero=input("Numero de 4 cifras: ")
+
+numero_invertido= numero[3] + numero[2] + numero [1] + numero[0]
+
+print(f"Numero invetido: {numero_invertido}")
