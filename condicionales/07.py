65b994956475449e96a432e6238a75f875bd49b8
CristianQSS8113 <cristianeduardo1812@gmail.com>
2024-10-13 00:26:30 -0500

    07

---
+++ condicionales/07.py
@@ -0,0 +1,14 @@
+numero1 = int(input("Primer número: "))
+numero2 = int(input("Segundo número: "))
+numero3 = int(input("Tercer número: "))
+
+numero_intermedio=numero1
+
+if (numero1 > numero2 and numero1 < numero3) or (numero1 < numero2 and numero1 > numero3):
+    numero_intermedio = numero1
+elif (numero2 > numero1 and numero2 < numero3) or (numero2 < numero1 and numero2 > numero3):
+    numero_intermedio=numero2
+else:
+    numero_intermedio=numero3
+    
+print(f"Número intermedio es: {numero_intermedio}")
