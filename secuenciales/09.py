2221b8c8f0e6b95862a315fb294a565aee512ef2
CristianQSS8113 <cristianeduardo1812@gmail.com>
2024-10-12 19:26:40 -0500

    09

--- secuenciales/09.py
+++ secuenciales/09.py
@@ -1,3 +1,15 @@
 import os
 os.system("cls")
 
+numero=int(input("Numero de 4 cifras: "))
+
+c1 = numero // 1000
+numero %= 1000 
+c2 = numero // 100
+numero %=100
+c3 = numero //10
+c4 = numero % 10
+
+suma=c1 + c2 +c3 + c4
+
+print(f"Suma = {suma}") 
